70779d844e4c2af3f3350d5e1cafd08cca4488c2
EstebanEB <estebanlolequisde@gmail.com>
2018-12-07 02:27:56 -0600

    Quiz de historia
    Quiz de historia
    *bug a la tercera face*

---
+++ Gyro/Assets/quizGame/Scripts/bttnFunc.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bttnFunc : MonoBehaviour {
+
+
+
+    public void ButtonFunc()
+    {
+
+        GameObject controller = GameObject.FindGameObjectWithTag("GameController");
+
+        controller.GetComponent<AnwerManager>().AnswerChecker(this.name);
+
+    }
+}
